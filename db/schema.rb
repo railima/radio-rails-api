f9681d6c16d007427f3c75a064407ae157071543
Rai <raioliveira.ec@gmail.com>
2022-07-16 18:05:05 -0300

    Change schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_16_195713) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_16_210239) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,4 +40,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_16_195713) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "titulo"
+    t.text "conteudo"
+    t.datetime "publicado_em"
+    t.bigint "admin_user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["admin_user_id"], name: "index_posts_on_admin_user_id"
+  end
+
+  add_foreign_key "posts", "admin_users"
 end
